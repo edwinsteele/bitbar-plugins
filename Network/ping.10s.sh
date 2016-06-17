15032fb686b929afacc767aa34f6e1929f2f3bf9
Edwin Steele <edwin-lists@wordspeak.org>
2016-06-18 06:27:28 +1000

    Show avg and std dev of accessible sites

--- Network/ping.10s.sh
+++ Network/ping.10s.sh
@@ -8,9 +8,13 @@
 # Authors: (Trung Đinh Quang) trungdq88@gmail.com and (Grant Sherrick) https://github.com/thealmightygrant
 
 MAX_PING=10000
-SITES=(google.com youtube.com wikipedia.org github.com imgur.com)
+SITES=(www.wordspeak.org gemini.wordspeak.org bigpond.com google.com tpg.com.au 10.20.20.41)
+
+HAS_UNREACHABLE_SITE=0
+UNREACHABLE_PING_TIME=-1
 
 #grab ping times for all sites
+REACHABLE_SITE_COUNT=0
 SITE_INDEX=0
 PING_TIMES=
 
@@ -18,7 +22,10 @@ while [ $SITE_INDEX -lt ${#SITES[@]} ]; do
     NEXT_SITE="${SITES[$SITE_INDEX]}"
     NEXT_PING_TIME=$(ping -c 2 -n -q "$NEXT_SITE" 2>/dev/null | awk -F '/' 'END {printf "%d\n", $5}')
     if [ "$NEXT_PING_TIME" -eq 0 ]; then
-        NEXT_PING_TIME=$MAX_PING
+        NEXT_PING_TIME=$UNREACHABLE_PING_TIME
+        HAS_UNREACHABLE_SITE=1
+    else
+        REACHABLE_SITE_COUNT=$(( $REACHABLE_SITE_COUNT + 1 ))
     fi
     if [ -z "$PING_TIMES" ]; then
         PING_TIMES=($NEXT_PING_TIME)
@@ -28,24 +35,33 @@ while [ $SITE_INDEX -lt ${#SITES[@]} ]; do
     SITE_INDEX=$(( $SITE_INDEX + 1 ))
 done
 
-# Calculate the average ping
-SITE_INDEX=0
-AVG=0
-while [ $SITE_INDEX -lt ${#SITES[@]} ]; do
-    AVG=$(( ($AVG + ${PING_TIMES[$SITE_INDEX]}) ))
-    SITE_INDEX=$(( $SITE_INDEX + 1 ))
-done
-AVG=$(( $AVG / ${#SITES[@]} ))
-
-# Calculate STD dev
-SITE_INDEX=0
-AVG_DEVS=0
-while [ $SITE_INDEX -lt ${#SITES[@]} ]; do
-    AVG_DEVS=$(( $AVG_DEVS + (${PING_TIMES[$SITE_INDEX]} - $AVG)**2 ))
-    SITE_INDEX=$(( $SITE_INDEX + 1 ))
-done
-AVG_DEVS=$(( $AVG_DEVS / ${#SITES[@]} ))
-SD=$(echo "sqrt ( $AVG_DEVS )" | bc -l | awk '{printf "%d\n", $1}')
+# Calculate the average ping for reachable sites
+if [ $REACHABLE_SITE_COUNT -gt 0 ]; then
+    SITE_INDEX=0
+    AVG=0
+    while [ $SITE_INDEX -lt ${#SITES[@]} ]; do
+        if [ ${PING_TIMES[$SITE_INDEX]} -gt $UNREACHABLE_PING_TIME ]; then
+            AVG=$(( ($AVG + ${PING_TIMES[$SITE_INDEX]}) ))
+        fi
+        SITE_INDEX=$(( $SITE_INDEX + 1 ))
+    done
+    AVG=$(( $AVG / $REACHABLE_SITE_COUNT ))
+    
+    # Calculate STD dev
+    SITE_INDEX=0
+    AVG_DEVS=0
+    while [ $SITE_INDEX -lt ${#SITES[@]} ]; do
+        if [ ${PING_TIMES[$SITE_INDEX]} -gt $UNREACHABLE_PING_TIME ]; then
+            AVG_DEVS=$(( $AVG_DEVS + (${PING_TIMES[$SITE_INDEX]} - $AVG)**2 ))
+        fi
+        SITE_INDEX=$(( $SITE_INDEX + 1 ))
+    done
+    AVG_DEVS=$(( $AVG_DEVS / $REACHABLE_SITE_COUNT ))
+    SD=$(echo "sqrt ( $AVG_DEVS )" | bc -l | awk '{printf "%d\n", $1}')
+else
+    # Make it so that we get a skull and crossbones
+    AVG=$MAX_PING
+fi
 
 # Define color
 COLOR="#cc3b3b"
@@ -68,12 +84,18 @@ else
     COLOR="#e506ff"
 fi
 
-echo "$MSG | font=UbuntuMono-Bold color=$COLOR size=10"
+if [ $HAS_UNREACHABLE_SITE -eq 1 ]; then
+    FONT="Impact"
+else
+    FONT="Georgia"
+fi
+
+echo "$MSG | font="$FONT" color=$COLOR size=10"
 echo "---"
 SITE_INDEX=0
 while [ $SITE_INDEX -lt ${#SITES[@]} ]; do
     PING_TIME=${PING_TIMES[$SITE_INDEX]}
-    if [ $PING_TIME -eq $MAX_PING ]; then
+    if [ $PING_TIME -eq $UNREACHABLE_PING_TIME ]; then
         PING_TIME="☠"
     else
         PING_TIME="$PING_TIME ms"
